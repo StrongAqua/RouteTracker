e735d270245a27b3acd7b5e1d1f5ed7c8846e8ae
Alla <prirez@bk.ru>
2021-06-21 18:14:22 +0300

    Homework 8
      - added: making selfie with UIImagePickerController
      - added: saving selfie
      - added: custom marker on map

--- RouteTracker/App/Flows/Map/MapViewController.swift
+++ RouteTracker/App/Flows/Map/MapViewController.swift
@@ -20,17 +20,17 @@ class MapViewController: UIViewController {
     @IBOutlet weak var startTrackingButton: UIButton!
     
     var locationManager = LocationManager.instance
-
+    
     // for the current position view
     var marker: GMSMarker?
-
+    
     // for the tracking feature
     var isTrackingStarted = false
     var route = GMSPolyline()
     var routePath = GMSMutablePath()
     
     let disposeBag = DisposeBag()
-
+    
     @IBOutlet weak var mapView: GMSMapView!
     
     override func viewDidLoad() {
@@ -39,9 +39,9 @@ class MapViewController: UIViewController {
         route.strokeWidth = 3
         route.strokeColor = .green
         route.map = mapView
-
+        
         configureLocationManager()
-
+        
         if loadRoute() {
             let position = GMSCameraPosition.camera(
                 withTarget: routePath.coordinate(at: routePath.count() - 1),
@@ -51,19 +51,6 @@ class MapViewController: UIViewController {
         } else {
             locationManager.currentLocation()
         }
-    }
-    
-    func showCurrentLocation(position: CLLocationCoordinate2D) {
-        let rect = CGRect(x: 0, y: 0, width: 20, height: 20)
-        let view = UIView(frame: rect)
-
-        view.layer.cornerRadius = 10
-        view.backgroundColor = .blue
-        
-        let marker = GMSMarker(position: position)
-        marker.iconView = view
-        marker.map = mapView
-        mapView.camera = GMSCameraPosition.camera(withTarget: position, zoom: 17)
     }
     
     func configureLocationManager() {
@@ -81,7 +68,7 @@ class MapViewController: UIViewController {
                 } else {
                     debugPrint("LOCATION: Location updated")
                 }
-
+                
                 let position = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 17)
                 self.mapView.animate(to: position)
                 
@@ -89,8 +76,8 @@ class MapViewController: UIViewController {
             })
         subscription.disposed(by: disposeBag)
     }
-
-
+    
+    
     func loadRoute() -> Bool {
         guard let realm = try? Realm() else {
             debugPrint("ERROR: can't use Realm")
@@ -102,7 +89,7 @@ class MapViewController: UIViewController {
             debugPrint("REALM: No path found in db.")
             return false
         }
-
+        
         routePath.removeAllCoordinates()
         for point in realmPath.coordinates {
             routePath.add(
@@ -111,17 +98,17 @@ class MapViewController: UIViewController {
         }
         route.path = routePath
         showPathButton.isEnabled = true
-
+        
         if routePath.count() > 0 {
             showMarker(position: routePath.coordinate(at: routePath.count() - 1))
             debugPrint("REALM: Path loaded, \(routePath.count()) point(s).")
             return true
         }
-
+        
         debugPrint("REALM: Loaded path is empty.")
         return false
     }
-
+    
     func saveCurrentRoute() {
         guard let realm = try? Realm() else {
             debugPrint("ERROR: can't use Realm")
@@ -147,7 +134,7 @@ class MapViewController: UIViewController {
             debugPrint("REALM: Cleanup.")
             realm.add(realmPath)
             debugPrint("REALM: Path saved.")
-
+            
             debugPrint("REALM: Update UI")
             self?.showPathButton.isEnabled = true
         }
@@ -158,15 +145,15 @@ class MapViewController: UIViewController {
         if isTrackingStarted {
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else {return}
-
+                
                 debugPrint("TRACKING: Start tracking location")
                 self.locationManager.startMonitoringSignificantLocationChanges()
                 self.locationManager.startUpdatingLocation()
-
+                
                 debugPrint("TRACKING: Cleanup current track points")
                 self.routePath.removeAllCoordinates()
                 self.route.path = self.routePath
-
+                
                 debugPrint("TRACKING: Update UI")
                 self.startTrackingButton.setTitle("Stop Tracking", for: .normal)
                 self.showPathButton.isEnabled = false
@@ -175,18 +162,18 @@ class MapViewController: UIViewController {
         else {
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else {return}
-
+                
                 debugPrint("TRACKING: Stop tracking location")
                 self.locationManager.stopMonitoringSignificantLocationChanges()
                 self.locationManager.stopUpdatingLocation()
-
+                
                 debugPrint("TRACKING: Store track points")
                 self.saveCurrentRoute()
-
+                
                 debugPrint("TRACKING: Cleanup current track points")
                 self.routePath.removeAllCoordinates()
                 self.route.path = self.routePath
-
+                
                 debugPrint("TRACKING: Update UI")
                 self.startTrackingButton.setTitle("Start Tracking", for: .normal)
             }
@@ -215,9 +202,23 @@ class MapViewController: UIViewController {
     }
     
     func showMarker(position: CLLocationCoordinate2D) {
+        let image = UIImage(
+            contentsOfFile: self.getDocumentsDirectory().appendingPathComponent("marker.png").path
+        )
+        
+        let rect = CGRect(x: 0, y: 0, width: 40, height: 40)
+        let view = UIView(frame: rect)
+        let imageView = UIImageView(frame: rect)
+        view.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = image
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        
         self.marker?.map = nil
         let marker = GMSMarker(position: position)
-        marker.icon = GMSMarker.markerImage(with: .blue)
+        marker.iconView = view
+        // marker.icon = image //GMSMarker.markerImage(with: .blue)
         marker.map = mapView
         self.marker = marker
     }
